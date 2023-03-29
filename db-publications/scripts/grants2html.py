9d259144c8e26bb71c4c5e5b12096c6dc7940648
Takaaki Aoki <takaaki.aoki.work@gmail.com>
2023-03-29 11:14:38 +0900

    fixed nomarkdown tag

--- db-publications/scripts/grants2html.py
+++ db-publications/scripts/grants2html.py
@@ -5,7 +5,7 @@ with open(sys.argv[1]) as f:
     db = json.load(f)
 
 
-print("{::nomarkdown}<ol>")
+print("{::nomarkdown}\n<ol>")
 for e in sorted(db, key= lambda k: k["year"], reverse=True):
     s = "<li> "
     s += "{year}年度 「{研究課題名}」<br> {研究種目}".format(**e)
@@ -13,12 +13,10 @@ for e in sorted(db, key= lambda k: k["year"], reverse=True):
         s += "・研究代表者"
     else:
         s += "・研究分担者"
-    if "金額" in e:
-        s += "・{金額}円".format(**e)
     if "url" in e:
         s += "<br> <a href={url}>課題番号 {ID}</a>".format(**e)
     print(s)
-print("</ol>{:/}\n\n")
+print("</ol>\n{:/nomarkdown}\n\n")
     
     
 
